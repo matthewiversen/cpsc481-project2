8e88c808f888e8cf094cc273ea0fa39c5fcdf63f
Linh Nguyen <lnguyen1148@csu.fullerton.edu>
2022-06-23 01:52:40 -0700

    terminal_test and utility function

--- game_of_nim.py
+++ game_of_nim.py
@@ -7,22 +7,38 @@ class GameOfNim(Game):
     a list with number of objects in each row."""
 
     def __init__(self, board=[3,1]):
-        raise NotImplementedError
+        moves = []
+        for row in range(len(board)):
+            for match_amount in range(1, board[row] + 1):
+                moves.append((row, match_amount))
+        
+        self.initial = GameState(to_move='MAX', utility=0, board=board, moves=moves)
 
     def actions(self, state):
         """Legal moves are at least one object, all from the same row."""
         return state.moves
 
     def result(self, state, move):
-        raise NotImplementedError
+        pass
+        # raise NotImplementedError
 
     def utility(self, state, player):
         """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
-        raise NotImplementedError
+        if terminal_test(state):
+                return 1 if player == "MAX" else -1
+
 
     def terminal_test(self, state):
         """A state is terminal if there are no objects left"""
-        raise NotImplementedError
+        num_of_objects = 0
+        for objects in len(state.board):
+            num_of_objects += objects
+
+        if num_of_objects == 0:
+            return True
+        else:
+            return False
+
 
     def display(self, state):
         board = state.board
@@ -32,11 +48,14 @@ class GameOfNim(Game):
 if __name__ == "__main__":
     nim = GameOfNim(board=[0, 5, 3, 1]) # Creating the game instance
     #nim = GameOfNim(board=[7, 5, 3, 1]) # a much larger tree to search
+
     print(nim.initial.board) # must be [0, 5, 3, 1]
     print(nim.initial.moves) # must be [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2, 1), (2, 2), (2, 3), (3, 1)]
-    print(nim.result(nim.initial, (1,3) ))
-    utility = nim.play_game(alpha_beta_player, query_player) # computer moves first 
-    if (utility < 0):
-        print("MIN won the game")
-    else:
-        print("MAX won the game")
+    
+    # print(nim.result(nim.initial, (1,3) ))
+
+    # utility = nim.play_game(alpha_beta_player, query_player) # computer moves first 
+    # if (utility < 0):
+    #     print("MIN won the game")
+    # else:
+    #     print("MAX won the game")
